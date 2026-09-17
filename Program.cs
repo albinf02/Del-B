0c86ddb642c95ce1e76560d4288c415e331fdf91
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 21:27:44 +0200

    renade upp koden mer

--- Program.cs
+++ Program.cs
@@ -3,15 +3,22 @@
 Student Albin = new Student ("Albin");
 Student Filip = new Student ("Filip");
 
-Albin.AddCourse(english);
-Albin.AddCourse(english);
+bool firstRegistration = Albin.AddCourse(english);
+bool secondRegistration = Albin.AddCourse(english);
+
+Console.WriteLine($"First registration: {firstRegistration}");
+Console.WriteLine($"Second registration: {secondRegistration}");
+
 english.AddStudent(Filip);
+
 Student Tomas = new Student("Tomas");
+
 bool added = english.AddStudent(Tomas);
 
 Console.WriteLine($"Tomas added: {added}");
-
 Console.WriteLine("Students in the Course: ");
+
+
 
 foreach (Student student in english.Students)
 {
@@ -20,14 +27,13 @@ foreach (Student student in english.Students)
 bool removed = english.RemoveStudent(Albin);
 
 Console.WriteLine($"Albin removed: {removed}");
-
 Console.WriteLine("Courses for Albin:");
+
 
 foreach (Course course in Albin.Courses)
 {
     Console.WriteLine(course.Name);
 }
-
 Console.WriteLine("Students in the Course:");
 
 foreach (Student student in english.Students)
@@ -35,5 +41,4 @@ foreach (Student student in english.Students)
     Console.WriteLine(student.Name);
 }
 bool removedTomas = english.RemoveStudent(Tomas);
-
 Console.WriteLine($"Tomas removed: {removedTomas}");
