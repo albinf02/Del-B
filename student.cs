67015f593d687ce3c134575f3bd2a9fa061709e8
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 20:48:03 +0200

    la till AddCourse för Student

--- student.cs
+++ student.cs
@@ -1,16 +1,32 @@
-
 public class Student
 {
-    public string Name {get; }
+   public string Name {get; }
     
     //kurser som som studenten har
    public List<Course> Courses { get; } = new List<Course>();
 
-
-
-    public Student(string name)
-
+   public Student(string name)
     {
         Name = name;
     }
-}
+    public bool AddCourse(Course course)
+    {
+        if (Courses.Contains(course))
+        {
+            return false;
+        }
+        if (course.Students.Count >= course.Capacity)
+        {
+            return false;
+        }
+        Courses.Add (course);
+
+        if (!course.Students.Contains(this))
+        {
+            course.Students.Add(this);
+        }
+      return true;
+    }
+ }
+    
+
