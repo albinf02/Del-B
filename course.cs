50d0cba52ac2d4f353d42839c26695e361a08e34
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 20:19:43 +0200

    skapa grunden för del B  (course.cs, student.cs)

---
+++ course.cs
@@ -0,0 +1,14 @@
+public class Course
+
+{
+    public string Name {get; }
+    public int Capacity { get;}
+
+    public Course (String name, int capacity)
+
+    {
+        Name = name;
+        Capacity = capacity;
+        
+    }
+}
---
+++ student.cs
@@ -0,0 +1,10 @@
+public class Student
+{
+    public string Name {get; }
+    
+    public Student(string name)
+
+    {
+        Name = name;
+    }
+}
