6a46a94eed8228d9c5fb7dac2dc8f8dc631237ce
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 21:09:16 +0200

    kontrolla att bara två studenter kan gå i  klassen

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ Student Albin = new Student ("Albin");
 Student Filip = new Student ("Filip");
 
 Albin.AddCourse(english);
-Albin.AddCourse(english);
 english.AddStudent(Filip);
+Student Tomas = new Student("Tomas");
+bool added = english.AddStudent(Tomas);
+
+Console.WriteLine($"Tomas added: {added}");
+
 Console.WriteLine("Students in the Course: ");
 
 foreach (Student student in english.Students)
