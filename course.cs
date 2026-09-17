0112cc4cc785df36aee02bb7746f36666f6de41e
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 20:30:09 +0200

    studenter som är anmälda i course.cs

--- course.cs
+++ course.cs
@@ -1,14 +1,12 @@
 public class Course
-
 {
-    public string Name {get; }
-    public int Capacity { get;}
-
-    public Course (String name, int capacity)
-
+    public string Name { get; }
+    public int Capacity { get; }
+    // studenter som är anmälda
+public List<Student> Students {get;} = new List<Student<Student>();
+    public Course(string name, int capacity)
     {
         Name = name;
         Capacity = capacity;
-        
     }
 }
