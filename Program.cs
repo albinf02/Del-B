3c207f5a2af755c0e01b4f20cae7bd8918c22ede
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 20:57:30 +0200

    la till koppling mellan kurs och student

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Course english = new Course("English", 2);
+
+Student Albin = new Student ("Albin");
+Student Filip = new Student ("Filip");
+
+Albin.AddCourse(english);
+english.AddStudent(Filip);
+Console.WriteLine("Students in the Course: ");
+
+foreach (Student student in english.Students)
+{
+    Console.WriteLine(student.Name);
+}
