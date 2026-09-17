95b5d034fc97680948d37c1b3c9b72d1b7469372
albinafk <fredrikssonalbin02@hotmail.com>
2026-09-17 20:40:32 +0200

    Lägg till studentens kurslista

--- course.cs
+++ course.cs
@@ -3,10 +3,30 @@ public class Course
     public string Name { get; }
     public int Capacity { get; }
     // studenter som är anmälda
-public List<Student> Students {get;} = new List<Student<Student>();
+public List<Student> Students { get; } = new List<Student>();
     public Course(string name, int capacity)
     {
         Name = name;
         Capacity = capacity;
     }
+
+public bool AddStudent(Student student)
+{
+        if (Students.Contains(student))
+        {
+            return false;
+        }
+    if (Students.Count >= Capacity)
+        {
+            return false;
+        }
+        Students.Add(student);
+
+        if (!student.Courses.Contains(this))
+        {
+            student.Courses.Add(this);
+        }
+        return true;
+    }
+
 }
--- student.cs
+++ student.cs
@@ -1,7 +1,13 @@
+
 public class Student
 {
     public string Name {get; }
     
+    //kurser som som studenten har
+   public List<Course> Courses { get; } = new List<Course>();
+
+
+
     public Student(string name)
 
     {
